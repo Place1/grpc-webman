39d725c28308f6e12d7699dcd4bb46d9db0af862
James Batt <getjamesbatt@gmail.com>
2020-05-18 21:48:29 +1000

    added release script

--- package.sh
+++ package.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 wails build -p -x "darwin/amd64"
+tar czf build/grpc-webman-darwin-amd64.app.tar.gz build/GRPCWebman.app
+
 wails build -p -x "linux/amd64"
 wails build -p -x "windows/amd64"
---
+++ release.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -eou pipefail
+
+REPO="grpc-webman"
+
+if ! [ -x "$(command -v github-release)" ]; then
+  echo 'please install github-release using "go get github.com/aktau/github-release"'
+fi
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo 'please set the $GITHUB_TOKEN variable'
+  exit 1
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+read -p 'GitHub Username: ' USERNAME
+
+echo "The current release is: $(git tag --list | tail -n 1)"
+read -p 'Tag (e.g. v1.0.0): ' TAG
+
+echo "building..."
+./package.sh
+
+echo "tagging release in git"
+git tag -a "$TAG" -m "$TAG"
+git push --tags
+
+echo "creating release"
+github-release release \
+  --user "$USERNAME" \
+  --repo "$REPO" \
+  --tag "$TAG" \
+  --name "$TAG"
+
+echo "uploading artifacts"
+ARTIFACTS=(
+  "grpc-webman-darwin-amd64.app.tar.gz"
+  "grpc-webman-darwin-10.6-amd64"
+  "grpc-webman-linux-amd64"
+  "grpc-webman-windows-4.0-amd64.exe"
+)
+for ARTIFACT in "${ARTIFACTS[@]}"; do
+  echo "uploading: $ARTIFACT"
+  github-release upload \
+    --user "$USERNAME" \
+    --repo "$REPO" \
+    --tag "$TAG" \
+    --name "$ARTIFACT" \
+    --file "./build/$ARTIFACT"
+done
+
+echo "done"
