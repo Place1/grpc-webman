4b93a5365b904032b5d2e4c24278503348aa99b9
James Batt <getjamesbatt@gmail.com>
2020-05-19 19:08:57 +1000

    wip

--- package.sh
+++ package.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+# clean
+rm -r ./build/ || true
+
+# package for all platforms
 wails build -p -x "darwin/amd64"
 tar czf build/grpcwebman-darwin-amd64.app.tar.gz build/GRPCWebman.app
-
 wails build -p -x "linux/amd64"
 wails build -p -x "windows/amd64"
